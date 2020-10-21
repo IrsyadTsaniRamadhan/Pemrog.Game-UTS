f5de37b3b2c203155d457e451d96b74e73e742ea
IrsyadTsaniRamadhan <42790631+IrsyadTsaniRamadhan@users.noreply.github.com>
2020-10-21 11:49:50 +0800

    UTS Pemrograman Game

---
+++ Assets/Script/HalamanUtama.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HalamanUtama : MonoBehaviour
+{
+    public void pindahHalaman(string halaman){
+        SceneManager.LoadScene(halaman);
+    }
+
+    public void Keluar(){
+        Application.Quit();
+    }
+}
